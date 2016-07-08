d38ee779759a3f882c9148078ad52caee15e695a
Djumpion <962306591@qq.com>
2016-07-08 12:24:20 -0700

    lesson_4

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,14 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
 	STATUS_CHOICE = (("draft","DRAFT"),("published","PUBLISHED"),)
 	title = models.CharField(max_length=250)
 	slug = models.SlugField(max_length=250,unique_for_date='published')
 	author = models.ForeignKey(User,related_name = "blog_posts")
 	body = models.TextField()
 	published = models.DateTimeField(default=timezone.now)
-	updated = models.DateTimeField(auto_now_add=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10,choices=STATUS_CHOICE,default="draft")
 	
 	class Meta:
