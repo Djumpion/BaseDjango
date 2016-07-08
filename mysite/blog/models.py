848a4dc99f18ba5c9fbe53eb16bf83448edd2996
Djumpion <962306591@qq.com>
2016-07-08 09:56:33 -0700

    lesson3 add_adminsite

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
 	STATUS_CHOICE = (("draft","DRAFT"),("published","PUBLISHED"),)
 	title = models.CharField(max_length=250)
 	slug = models.SlugField(max_length=250,unique_for_date='published')
